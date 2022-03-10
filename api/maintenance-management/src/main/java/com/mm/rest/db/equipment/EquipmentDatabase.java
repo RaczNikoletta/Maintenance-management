85238d09b764a8654775b7c8027e64ce99abc766
NorbertBurkus <95472985+NorbertBurkus@users.noreply.github.com>
2022-03-10 12:49:35 +0100

    próba

---
+++ api/maintenance-management/src/main/java/com/mm/rest/db/equipment/EquipmentDatabase.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mm.rest.db.equipment;
+
+/**
+ *
+ * @author burku
+ */
+public class EquipmentDatabase {
+    
+}
